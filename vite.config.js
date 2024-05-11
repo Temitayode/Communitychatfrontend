e61b7fe6d0824b11eddbbc17d80e4706c398c4a7
ajfahim <ajfahim52@gmail.com>
2024-05-12 01:24:19 +0600

    backend changed

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "http://localhost:5000",
-        target: "https://mern-chat-and-forum-application.onrender.com",
+        target: "https://mern-chat-and-forum-application-api.onrender.com",
       },
     },
   },
